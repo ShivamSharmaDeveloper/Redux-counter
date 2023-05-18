462f6315bfd31531b58ba6cceeada6cb1493e101
Shivam Sharma <shivsharma7821@gmail.com>
2023-05-18 16:40:18 +0530

    🚀 chore(package.json): add @reduxjs/toolkit dependency to the project 🎨 refactor(Counter.js): replace plain action objects with action creators from the counterSlice to improve code readability and maintainability 🎨 refactor(index.js): replace plain reducer function with a reducer created using createSlice from @reduxjs/toolkit to improve code readability and maintainability

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,21 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
+import { counterActions } from "../store/index";
 
 const Counter = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
   const show = useSelector((state) => state.showCounter);
   const incrementHandler = () => {
-    dispatch({type: 'increment'})
-  }
+    dispatch(counterActions.increment());
+  };
   const increaseHandler = () => {
-    dispatch({type: 'increase', amount: 10})
-  }
+    dispatch(counterActions.increase(10)); // { type: SOME-UNIQUE-IDENTIFIER, payload: 10}
+  };
   const decrementHandler = () => {
-    dispatch({type: 'decrement'})
-  }
+    dispatch(counterActions.decrement());
+  };
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
   return (
     <main className={classes.counter}>
